9b26b063e99fb766f27af01f8be396799ed98edd
Sowmen Das <sowmendipta@gmail.com>
2020-01-16 21:02:16 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/painter/DrawView.java
@@ -0,0 +1,325 @@
+package com.example.painter;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class DrawView extends View
+{
+    //public vars
+    public Bitmap canvasBitmap;
+
+    //private vars
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xFF660000;
+    private Canvas drawCanvas;
+    private float brushSize, lastBrushSize;
+    private boolean erase = false;
+
+    private Integer currentBrushSize = 5;
+    private DrawView drawView;
+    private ArrayList<Path> mPaths;
+    private ArrayList<Path> undonePaths = new ArrayList<>();
+    private ArrayList<Paint> undonePaints = new ArrayList<>();
+    private ArrayList<Paint> mPaints;
+
+    public boolean flagline = false;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDraw();
+    }
+
+    public void startNew() {
+        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        setupDraw();
+        invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    private void setupDraw()
+    {
+        this.mPaths = new ArrayList<Path>();
+        this.mPaints = new ArrayList<Paint>();
+
+        this.addPath(false);
+
+        drawPaint = new Paint();
+        drawPath = new Path();
+
+        drawPaint.setColor(paintColor);
+        drawPaint.setStrokeWidth(currentBrushSize);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+
+        drawPaint.setPathEffect(new CornerPathEffect(10) );
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+//        lastBrushSize = currentBrushSize;
+    }
+
+    public void setBrushSize(float newSize) {
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, getResources().getDisplayMetrics());
+
+        brushSize = pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
+    }
+
+    public void setLastBrushSize(float lastSize)
+    {
+        lastBrushSize = lastSize;
+    }
+
+    public float getLastBrushSize()
+    {
+        return lastBrushSize;
+    }
+
+    public void setErase(boolean isErase) {
+        erase = isErase;
+
+        if (erase)
+        {
+            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        }
+        else
+        {
+            drawPaint.setXfermode(null);
+        }
+    }
+
+
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+//        canvas.drawPath(drawPath, drawPaint);
+
+        for (int i = 0; i < mPaths.size(); ++i) {
+            canvas.drawPath(mPaths.get(i), mPaints.get(i));
+            invalidate();
+        }
+    }
+
+    private void addPath(boolean fill)
+    {
+        drawPath = new Path();
+        mPaths.add(drawPath);
+
+        drawPaint = new Paint();
+        mPaints.add(drawPaint);
+        System.out.print(paintColor);
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(currentBrushSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        invalidate();
+
+    }
+
+    public void onClickUndo() {
+        if (mPaths.size() > 0) {
+            undonePaths.add(mPaths.remove(mPaths.size() - 1));
+            undonePaints.add(mPaints.remove(mPaints.size() - 1));
+            invalidate();
+        } else {}
+        if (mPaths.size() > 0) {
+            undonePaths.add(mPaths.remove(mPaths.size() - 1));
+            undonePaints.add(mPaints.remove(mPaints.size() - 1));
+            invalidate();
+        }
+        else{}
+    }
+
+    public void onClickRedo() {
+        if (undonePaths.size() > 0) {
+            mPaths.add(undonePaths.remove(undonePaths.size() - 1));
+            mPaints.add(undonePaints.remove(undonePaints.size() - 1));
+            invalidate();
+        }
+        else{}
+        if (undonePaths.size() > 0) {
+            mPaths.add(undonePaths.remove(undonePaths.size() - 1));
+            mPaints.add(undonePaints.remove(undonePaints.size() - 1));
+            invalidate();
+        }
+        else{}
+    }
+
+    public void drawLine(boolean flag) {
+        flagline = flag;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                this.addPath(true);
+                drawPath.moveTo(touchX, touchY);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+
+                drawPath.lineTo(touchX, touchY);
+
+                this.addPath(true);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                invalidate();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void changeBrushSize(int size) {
+        currentBrushSize = size;
+        invalidate();
+
+    }
+
+    public int getPaintColor() {
+        return paintColor;
+    }
+
+    public void setColor(String newColor) {
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+        invalidate();
+    }
+
+    public void saveDrawing() {
+        fixMediaDir();
+
+        if (checkPermissionREAD_EXTERNAL_STORAGE(getContext())) {
+            this.setDrawingCacheEnabled(true);
+            String imgSaved = MediaStore.Images.Media.insertImage(getContext().getContentResolver(), this.getDrawingCache(), UUID.randomUUID().toString() + ".png", "drawing");
+            if (imgSaved != null) {
+                Toast savedtoast = Toast.makeText(getContext().getApplicationContext(), "Drawing saved to Gallery", Toast.LENGTH_SHORT);
+                savedtoast.show();
+            } else {
+                Toast unsaved = Toast.makeText(getContext().getApplicationContext(), "Image could not saved", Toast.LENGTH_SHORT);
+                unsaved.show();
+            }
+            this.destroyDrawingCache();
+        }
+    }
+
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                }
+                break;
+            default:
+        }
+    }
+
+    void fixMediaDir() {
+        File sdcard = Environment.getExternalStorageDirectory();
+        if (sdcard != null) {
+            File mediaDir = new File(sdcard, "DCIM/Camera");
+            if (!mediaDir.exists()) {
+                mediaDir.mkdirs();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/painter/MainActivity.java
@@ -0,0 +1,225 @@
+package com.example.painter;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+
+//java imports
+
+
+public class MainActivity extends AppCompatActivity implements OnClickListener
+{
+    private DrawView draw;
+    private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn, redoBtn, undoBtn;
+    private float smallBrush, mediumBrush, largeBrush;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        draw = (DrawView)findViewById(R.id.drawing);
+
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.colors);
+
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null));
+
+        smallBrush = getResources().getInteger(R.integer.smallSize);
+        mediumBrush = getResources().getInteger(R.integer.mediumSize);
+        largeBrush = getResources().getInteger(R.integer.largeSize);
+
+        drawBtn = (ImageButton)findViewById(R.id.brushBtn);
+        drawBtn.setOnClickListener(this);
+
+        draw.setBrushSize(mediumBrush);
+
+        eraseBtn = (ImageButton)findViewById(R.id.eraseBtn);
+        eraseBtn.setOnClickListener(this);
+
+//        newBtn = (ImageButton)findViewById(R.id.newBtn);
+//        newBtn.setOnClickListener(this);
+
+        saveBtn = (ImageButton)findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(this);
+
+        redoBtn = findViewById(R.id.redoBtn);
+        undoBtn = findViewById(R.id.undoBtn);
+
+        redoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                draw.onClickRedo();
+            }
+        });
+        undoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                draw.onClickUndo();
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},  112);
+        }
+    }
+
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.brushBtn)
+        {
+            final Dialog brushDialog = new Dialog(this, R.style.CustomDialog);
+            brushDialog.setTitle("Brush Size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(false);
+                    draw.setBrushSize(smallBrush);
+                    draw.setLastBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(false);
+                    draw.setBrushSize(mediumBrush);
+                    draw.setLastBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(false);
+                    draw.setBrushSize(largeBrush);
+                    draw.setLastBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+        else if (view.getId() == R.id.eraseBtn)
+        {
+            final Dialog brushDialog = new Dialog(this, R.style.CustomDialog);
+            brushDialog.setTitle("Eraser Size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(true);
+                    draw.setBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(true);
+                    draw.setBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    draw.setErase(true);
+                    draw.setBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+//        else if (view.getId() == R.id.newBtn)
+//        {
+//            AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+//            newDialog.setTitle("New Drawing");
+//            newDialog.setMessage("Start a new drawing? (This will erase your current drawing.)");
+//            newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    draw.startNew();
+//                    dialog.dismiss();
+//                }
+//            });
+//            newDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.cancel();
+//                }
+//            });
+//
+//            newDialog.show();
+//        }
+        else if (view.getId() == R.id.saveBtn)
+        {
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save Drawing");
+            saveDialog.setMessage("Save drawing to device gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    draw.saveDrawing();
+                }
+            });
+            saveDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
+        }
+    }
+
+    public void colorClicked(View view)
+    {
+        if (view != currPaint)
+        {
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+
+            draw.setErase(false);
+            draw.setBrushSize(draw.getLastBrushSize());
+            draw.setColor(color);
+
+            imgView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null));
+            currPaint.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.paint, null));
+            currPaint = (ImageButton)view;
+        }
+    }
+}
